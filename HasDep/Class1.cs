d3e9c61e2ca87bb6485ba404ca7ab0d9133c6616
Colin S. Gordon <csgordon@drexel.edu>
2020-12-01 11:01:15 -0500

    small repro

---
+++ HasDep/Class1.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CSharp;
+using Microsoft.CodeAnalysis.Text;
+using Pidgin;
+
+namespace HasDep
+{
+    [Generator]
+    public class Class1 : ISourceGenerator
+    {
+        public void Execute(GeneratorExecutionContext context) {
+            // use Pidgin
+            try {
+                Pidgin.Parser.Digit.ParseOrThrow("3");
+            } catch (ParseException p) {
+                // ignore
+            }
+        }
+        public void Initialize(GeneratorInitializationContext context) {
+            // nothing
+        }
+    }
+}
